725acb980beccde2a291d83d9b8ae5e1e00e0c3e
Andre Meyering <info@andremeyering.de>
2013-07-16 14:51:38 +0200

    new docs
    new docs

---
+++ js/docs-setup.js
@@ -0,0 +1,29 @@
+NG_DOCS={
+  "sections": {
+    "guide": "Guide"
+  },
+  "pages": [
+    {
+      "section": "guide",
+      "id": "index",
+      "shortName": "Guide",
+      "type": "overview",
+      "keywords": "$i18nextprovider angular angularjs app as bower checkout com config currently de dependency development false for full git guide how http i18next in inject install into is isn jm json lng module more myapp ng-i18next note options overview please quick read repository resgetpath right sometext start tell that the this title to translate under use usecookie uselocalstorage via visit way will with you your"
+    },
+    {
+      "section": "guide",
+      "id": "installation",
+      "shortName": "Installation",
+      "type": "overview",
+      "keywords": "coming guide installation overview soon"
+    }
+  ],
+  "apis": {
+    "guide": false
+  },
+  "html5Mode": false,
+  "startPage": "/guide",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
